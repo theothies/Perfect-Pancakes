0c23aa0d165a8e71011443200257dfb274422b76
Theodor Thiessen <theodor.thiessen@mhp.com>
2019-10-08 10:36:15 +0200

    changed MainActivity and fragment home xml

--- app/src/main/java/com/example/perfectpancakes/MainActivity.java
+++ app/src/main/java/com/example/perfectpancakes/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.perfectpancakes;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,6 +9,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import org.w3c.dom.Text;
 
 import java.text.DecimalFormat;
 
@@ -33,36 +31,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        Button calculate = (Button) findViewById(R.id.calculate);
-        calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                EditText diameter = (EditText) findViewById(R.id.diameter);
-                EditText thickness = (EditText) findViewById(R.id.thickness);
-                EditText number = (EditText) findViewById(R.id.number);
-                TextView result = (TextView) findViewById(R.id.resultPancakes);
-
-                double dia = Double.parseDouble(diameter.getText().toString());
-                double thic = Double.parseDouble(thickness.getText().toString());
-                int num = Integer.parseInt(number.getText().toString());
-                double res = ((dia * dia) * thic * Math.PI * 1.06 * num) / 4;
-                double egg = res * 0.004;
-                double milk = res * 0.412;
-                double butter = res * 0.103;
-                double flour = res * 0.227;
-                double water = res * 0.155;
-
-                //result.setText("You need to make " + res + "ml of batter");
-                result.setText("You need to make\n " + formatter.format(res) + "ml of pancake-batter\n"
-                        + "You need the following ingredients:\n"
-                        + formatter.format(egg) + " eggs\n"
-                        + formatter.format(milk) + " ml of milk\n"
-                        + formatter.format(butter) + " g of butter\n"
-                        + formatter.format(flour) + " g of plain flour\n"
-                        + formatter.format(water) + " ml of water\n"
-                        + "And a pinch of salt. Enjoy!");
-            }
-        });
     }
 
 }
--- app/src/main/java/com/example/perfectpancakes/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/perfectpancakes/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,9 +16,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.perfectpancakes.R;
 
+import java.text.DecimalFormat;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    DecimalFormat formatter = new DecimalFormat("#");
+    public Button calculate;
+    public EditText diameter;
+    public EditText thiccness;
+    public EditText number;
+    public TextView result;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,4 +42,37 @@ public class HomeFragment extends Fragment {
         });
         return root;
     }
+
+    public void calculatePancakeRecipe (View root){
+          calculate = root.findViewById(R.id.calculate);
+          diameter = root.findViewById(R.id.diameter);
+          thiccness = root.findViewById(R.id.thickness);
+          number = root.findViewById(R.id.number);
+          result = root.findViewById(R.id.resultPancakes);
+
+          calculate.setOnClickListener(new View.OnClickListener() {
+
+              public void onClick(View root){
+                  double dia =Double.parseDouble(diameter.getText().toString());
+                  double thic = Double.parseDouble(thiccness.getText().toString());
+                  int num = Integer.parseInt(number.getText().toString());
+                  double res = ((dia * dia) * thic * Math.PI * 1.06 * num)/4;
+                  double egg = res * 0.004;
+                  double milk = res * 0.412;
+                  double butter = res * 0.103;
+                  double flour = res * 0.227;
+                  double water = res * 0.155;
+
+                  result.setText("You need to make\n " + formatter.format(res) + "ml of pancake-batter\n"
+                          + "You need the following ingredients:\n"
+                          + formatter.format(egg) + " eggs\n"
+                          + formatter.format(milk) + " ml of milk\n"
+                          + formatter.format(butter) + " g of butter\n"
+                          + formatter.format(flour) + " g of plain flour\n"
+                          + formatter.format(water) + " ml of water\n"
+                          + "And a pinch of salt. Enjoy!");
+              }
+          });
+    }
+
 }
