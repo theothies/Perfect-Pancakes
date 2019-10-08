bdf0fbcb1bf0562e1adccdd59970e561f1c85b03
Theodor Thiessen <theodor.thiessen@mhp.com>
2019-10-08 09:32:29 +0200

    changed MainActivity and fragment home xml

--- app/src/main/java/com/example/perfectpancakes/MainActivity.java
+++ app/src/main/java/com/example/perfectpancakes/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.perfectpancakes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,7 +14,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    DecimalFormat formatter = new DecimalFormat("#");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,37 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        Button calculate = (Button) findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                EditText diameter = (EditText) findViewById(R.id.diameter);
+                EditText thickness = (EditText) findViewById(R.id.thickness);
+                EditText number = (EditText) findViewById(R.id.number);
+                TextView result = (TextView) findViewById(R.id.resultPancakes);
+
+                double dia = Double.parseDouble(diameter.getText().toString());
+                double thic = Double.parseDouble(thickness.getText().toString());
+                int num = Integer.parseInt(number.getText().toString());
+                double res = ((dia * dia) * thic * Math.PI * 1.06 * num) / 4;
+                double egg = res * 0.004;
+                double milk = res * 0.412;
+                double butter = res * 0.103;
+                double flour = res * 0.227;
+                double water = res * 0.155;
+
+                //result.setText("You need to make " + res + "ml of batter");
+                result.setText("You need to make\n " + formatter.format(res) + "ml of pancake-batter\n"
+                        + "You need the following ingredients:\n"
+                        + formatter.format(egg) + " eggs\n"
+                        + formatter.format(milk) + " ml of milk\n"
+                        + formatter.format(butter) + " g of butter\n"
+                        + formatter.format(flour) + " g of plain flour\n"
+                        + formatter.format(water) + " ml of water\n"
+                        + "And a pinch of salt. Enjoy!");
+            }
+        });
     }
 
 }
