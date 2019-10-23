e4bdb482a28941747b0722aee736cb776f461bc9
Enes Özdogan <enes.oezdogan2@gmail.com>
2019-10-23 10:04:26 +0200

    idk

--- app/src/main/java/com/example/perfectpancakes/PancakeListAdapter.java
+++ app/src/main/java/com/example/perfectpancakes/PancakeListAdapter.java
@@ -3,7 +3,6 @@ package com.example.perfectpancakes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
